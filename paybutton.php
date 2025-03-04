b0ad207579b3681ff97bf2aab236ab15b8b52729
David <Klakurka@users.noreply.github.com>
2025-03-03 22:03:58 -0800

    Version bump

--- paybutton.php
+++ paybutton.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: PayButton
  * Description: Monetize your content with configurable no-signup paywalls.
- * Version: 2.2.0
+ * Version: 2.3.0
  * Author: PayButton
  * Author URI:  https://github.com/PayButton/wordpress-plugin
  * License:     MIT
